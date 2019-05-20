e52365d8ed1432e3e9dfc193dc6c8daf950a79f8
fthvgb1 <fthvgb1@163.com>
2019-05-20 12:20:24 +0800

    fix

--- lib/cache/redis_.php
+++ lib/cache/redis_.php
@@ -1,36 +1,42 @@
 <?php
-class redis_{
-        private $redis;
 
-        function __construct($config = null){
-                $this->redis = new Redis();
-                if(empty($config)){
-                    $config = 'redis:6379';
-                }
-                list($host, $port) = explode(':', $config, 2);
-            $this->redis->pconnect($host, $port);
-            $this->redis->set('a', 'b');
+class redis_
+{
+    private $redis;
 
+    function __construct($config = null)
+    {
+        $this->redis = new Redis();
+        if (empty($config)) {
+            $config = 'redis:6379';
         }
+        list($host, $port) = explode(':', $config, 2);
+        $this->redis->pconnect($host, $port);
+        $this->redis->auth('fuck');
 
-        function get($key){
-                $gRefreshTime = $this->redis->get("OneIndex_gRefreshTime");
-                $key = "OneIndex_$gRefreshTime\_" . $key;
-                $data = $this->redis->get($key);
-                return unserialize($data) ?: null;
-        }
+    }
 
-        function set($key, $value=null, $expire=600){
-                $gRefreshTime = $this->redis->get("OneIndex_gRefreshTime");
-                if (empty($gRefreshTime)) {
-                        $gRefreshTime = time();
-                        $this->redis->set("OneIndex_gRefreshTime", $gRefreshTime);
-                }
-                $key = "OneIndex_$gRefreshTime\_" . $key;
-                return $this->redis->set($key, serialize($value), $expire);
-        }
+    function get($key)
+    {
+        $gRefreshTime = $this->redis->get("OneIndex_gRefreshTime");
+        $key = "OneIndex_$gRefreshTime\_" . $key;
+        $data = $this->redis->get($key);
+        return unserialize($data) ?: null;
+    }
 
-        function clear(){
-            $this->redis->set("OneIndex_gRefreshTime", time());
+    function set($key, $value = null, $expire = 600)
+    {
+        $gRefreshTime = $this->redis->get("OneIndex_gRefreshTime");
+        if (empty($gRefreshTime)) {
+            $gRefreshTime = time();
+            $this->redis->set("OneIndex_gRefreshTime", $gRefreshTime);
         }
+        $key = "OneIndex_$gRefreshTime\_" . $key;
+        return $this->redis->set($key, serialize($value), $expire);
+    }
+
+    function clear()
+    {
+        $this->redis->set("OneIndex_gRefreshTime", time());
+    }
 }
